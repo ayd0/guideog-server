c68b6748c336b29d748b7716ef3d52cf732bc8b3
ayd0 <73216273+ayd0@users.noreply.github.com>
2023-05-15 01:30:37 -0700

    typo

--- authenticate.js
+++ authenticate.js
@@ -41,7 +41,7 @@ exports.verifyAdmin = (req, res, next) => {
         return next();
     } else {
         const err = new Error(
-            "You are not authorized to perform this operatin!"
+            "You are not authorized to perform this operation!"
         );
         err.status = 403;
         return next(err);
