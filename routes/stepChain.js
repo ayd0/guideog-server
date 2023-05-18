0d958ca6333476ef56380bb90fe25da01456a61a
ayd0 <73216273+ayd0@users.noreply.github.com>
2023-05-17 21:43:09 -0700

    added error handling for route: stepChain PUT request, added DELETE for route: stepChain

--- routes/stepChain.js
+++ routes/stepChain.js
@@ -8,7 +8,7 @@ const cors = require("./cors");
 stepChainRouter
     .route("/")
     .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
-    .get(cors.corsWithOptions, (req, res, next) => {
+    .get(cors.cors, (req, res, next) => {
         Step.findById(req.stepId)
             .then((step) => {
                 res.statusCode = 200;
@@ -18,13 +18,22 @@ stepChainRouter
             .catch((err) => next(err));
     })
     .post((req, res, next) => {
-        const err = new Error(`Unable to post to step/${req.stepId}/stepChain/`);
+        const err = new Error(
+            `Unable to post to step/${req.stepId}/stepChain/`
+        );
         err.statusCode = 403;
         return next(err);
     })
-    .put(cors.corsWithOptions, (req, res, next) => {
+    .put([cors.corsWithOptions, authenticate.verifyUser], (req, res, next) => {
         Step.findById(req.stepId)
             .then((step) => {
+                console.log(step.stepChain.references[0].reference.toString() === req.body.reference);
+                if (step.stepChain.references.filter(ref => ref.reference.toString() === req.body.reference)) {
+                    console.log(`FOUND REFERENCE: ${req.body.reference} in STEP`);
+                    const err = new Error(`stepReference with reference value: ${req.body.reference} already exists in step: ${step._id}`);
+                    err.statusCode = 409;
+                    return next(err);
+                }
                 Step.findById(req.body.reference).then((refStep) => {
                     StepReference.create(req.body).then((stepReference) => {
                         step.stepChain.references.push(stepReference);
@@ -37,6 +46,37 @@ stepChainRouter
                 });
             })
             .catch((err) => next(err));
-    });
+    })
+    .delete(
+        [
+            cors.corsWithOptions,
+            authenticate.verifyUser,
+            authenticate.verifyAdmin,
+        ],
+        (req, res, next) => {
+            Step.findById(req.stepId)
+                .then((step) => {
+                    for (reference of step.stepChain.references) {
+                        StepReference.deleteOne({ _id: reference._id }).catch(
+                            (err) => next(err)
+                        );
+                    }
+                })
+                .catch((err) => next(err));
+
+            Step.updateOne(
+                { _id: req.stepId },
+                {
+                    $set: { stepChain: [] },
+                }
+            )
+                .then((response) => {
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "application/json");
+                    res.json(response);
+                })
+                .catch((err) => next(err));
+        }
+    );
 
 module.exports = stepChainRouter;
